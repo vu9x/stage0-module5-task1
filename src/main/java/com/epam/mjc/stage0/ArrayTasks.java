bc5c7cada1fa83e1f2690f6bde984f4da02502a3
Vu <nguyentuanvu9x@gmail.com>
2023-09-05 20:41:30 +0300

    Updating: ArrayTasks.findIndexOfNumber

--- src/main/java/com/epam/mjc/stage0/ArrayTasks.java
+++ src/main/java/com/epam/mjc/stage0/ArrayTasks.java
@@ -63,7 +63,7 @@ public class ArrayTasks {
     public int findIndexOfNumber(int[] arr, int number) {
         for (int i = 0; i < arr.length; i++) {
             if(arr[i] == number) {
-                return i+1;
+                return i;
             }
         }
 
